f2beccd18651e42d6228ad589ba70cc6facdb3ad
Sri Rahul <126871975+Sri-Rahul@users.noreply.github.com>
2024-11-01 01:56:15 +0530

    Add files via upload

--- app/components/other/PreLoader.tsx
+++ app/components/other/PreLoader.tsx
@@ -97,7 +97,7 @@ const PreLoader: React.FC = () => {
         <span>Web Developer</span>
         <span> / </span>
                
-        <span className="flex items-center justify-center gap-3"> Building  <Logo width={30} height={30} /> Eldora UI</span>
+        <span className="flex items-center justify-center gap-3"> Building  <Logo width={30} height={30} /> a better place</span>
         <div className="sub hidden"></div>
       </div>
     </div>
